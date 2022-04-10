67cc0baa2dfa59b898b16ced05a8df5c5109eb0e
Fatoş Dilara Fırtına <dilara_firtina@hotmail.com>
2022-04-10 17:53:28 +0300

    Assignment is completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const writeCards=(arr, eventType)=>{
+    const newArr = []
+    for (const name of arr) {
+        newArr.push(`Thank you, ${name}, for the wonderful ${eventType} gift!`)
+    }
+    console.log(newArr);
+    return newArr;
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
+const countDown=(positiveInt)=>{
+    if (positiveInt <= 0) {
+        return "It's not a positive integer!";           
+    }
+    while (positiveInt >= 0) {
+        console.log(positiveInt);
+        positiveInt--;
+    }
+}
+
+countDown(10);
